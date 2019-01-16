dbde66fab13c11b04bc6992301aea162056c1f13
Kyosuke Awata <kyou0209@gmail.com>
2019-01-16 18:05:09 +0900

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(other_title = "")
+    base_title = "Tutorial App"
+    if other_title.empty?
+      base_title
+    else
+      "#{other_title} | #{base_title}"
+    end
+  end
 end
