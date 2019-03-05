cf89f8797677552232cc88989bc45f85302068f8
Kyosuke Awata <llbzv396@ybb.ne.jp>
2019-03-05 12:10:07 +0900

    検索後のページでリロードした時にホーム画面へリダイレクト

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   post '/search/youtube', to: 'search#youtube'
   post '/search/rakuten', to: 'search#rakuten'
   post '/search/qiita',   to: 'search#qiita'
+  
+  get '/search/postal',  to: 'search#form'
+  get '/search/youtube', to: 'search#form'
+  get '/search/rakuten', to: 'search#form'
+  get '/search/qiita',   to: 'search#form'
 end
